44c92d08a4a90fcc533218f15bdfe59f58a47ae4
whyjsjejwj <s10257079@connect.np.edu.sg>
2025-11-24 23:32:23 +0800

    Changed one function, to return wrong value

--- calculator.py
+++ calculator.py
@@ -10,7 +10,7 @@ def subtract(a, b):
 
 def multiply(a, b):
     """Return the product of two numbers."""
-    return a * b
+    return False
 
 def divide(a, b):
     """Return the quotient of two numbers."""
