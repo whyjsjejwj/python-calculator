1ad506195e2ced1284c7fe648b10b2b4fc7a505e
whyjsjejwj <s10257079@connect.np.edu.sg>
2025-11-25 00:38:35 +0800

    Intermediate Practice Exercises

--- test_calculator.py
+++ test_calculator.py
@@ -2,17 +2,14 @@ import pytest
 from calculator import add, subtract, multiply, divide
 
 
-
 @pytest.mark.basic
 def test_add_basic():
     assert add(1, 2) == 3
 
 
-
 @pytest.mark.basic
 def test_subtract_basic():
     assert subtract(5, 3) == 2
-
 
 
 @pytest.mark.edge
@@ -21,11 +18,9 @@ def test_divide_by_zero():
         divide(1, 0)
 
 
-
 @pytest.mark.edge
 def test_large_numbers():
     assert multiply(100000, 200000) == 20000000000
-
 
 
 @pytest.mark.slow
